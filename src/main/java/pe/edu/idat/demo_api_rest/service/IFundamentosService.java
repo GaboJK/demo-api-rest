8bd488211f86b5b25a2a899d14e33a92a75aa0fa
GaboJK <sl70831885@idat.pe>
2025-02-20 10:01:21 -0500

    tercera peticion

--- src/main/java/pe/edu/idat/demo_api_rest/service/IFundamentosService.java
+++ src/main/java/pe/edu/idat/demo_api_rest/service/IFundamentosService.java
@@ -1,4 +1,14 @@
 package pe.edu.idat.demo_api_rest.service;
 
+import java.util.List;
+
 public interface IFundamentosService {
+
+    String validarNumeroPrimo(int numero);
+
+    String calcularFactorial(int numero);
+
+    List<Integer> calcularFibonacci(int numero);
+
+
 }
